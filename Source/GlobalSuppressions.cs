54736d25fd600165d5722fc67cc7cad0e947dad6
Andreas Pardeike <andreas@pardeike.net>
2020-02-28 09:39:40 +0100

    Release 2.0.1.2

--- Source/GlobalSuppressions.cs
+++ Source/GlobalSuppressions.cs
@@ -1,1 +1,2 @@
 ﻿[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ModCounter.Trigger")]
